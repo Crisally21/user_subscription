55b6bdb9bd1be83d8cabba8c29d43ab0a3b5324f
Владимир <vovaelandaev@gmail.com>
2025-05-06 18:58:46 +0300

    Добавил пропертисы, описал две модели данных

---
+++ src/main/java/org/web/task/user_subscription/model/User.java
@@ -0,0 +1,25 @@
+package org.web.task.user_subscription.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Subscription> subscriptions;
+}
