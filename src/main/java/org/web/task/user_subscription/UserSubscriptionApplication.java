b593aefeaffdaeffa695231ca922ce97c9068bd3
Владимир <vovaelandaev@gmail.com>
2025-05-06 18:34:17 +0300

    service create

---
+++ src/main/java/org/web/task/user_subscription/UserSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package org.web.task.user_subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSubscriptionApplication.class, args);
+    }
+
+}
